3ac535bcc792efbf45b9478b6fc29f3ecfbaf066
Altsoph <altsoph@gmail.com>
2018-02-14 23:03:50 +0300

    More usage examples added.

---
+++ example_run_fminpowell.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import community_ext
+import networkx as nx
+import scipy
+
+# optimization routine
+def opt_par(G,method,par):
+    def opt_fn(work_par):
+        work_par = work_par[0]
+        if method in ('ilfrs','ilfr'):
+            partition = community_ext.best_partition(G,model=method,pars={'mu':work_par})
+            loglike =   community_ext.model_log_likelihood(G,partition,model=method,pars={'mu':work_par})
+        else:
+            partition = community_ext.best_partition(G,model=method,pars={'gamma':work_par})
+            loglike =   community_ext.model_log_likelihood(G,partition,model=method,pars={'gamma':work_par})
+        return -loglike
+
+    # find the optimal parameter value
+    best_par = float(scipy.optimize.fmin_powell(opt_fn, par, full_output = False,disp = False))
+
+    # calculate the partition with the optimal parameter value
+    if method in ('ilfrs','ilfr'):
+        partition = community_ext.best_partition(G,model=method,pars={'mu':best_par})
+        loglike =   community_ext.model_log_likelihood(G,partition,model=method,pars={'mu':best_par})
+    else:
+        partition = community_ext.best_partition(G,model=method,pars={'gamma':best_par})
+        loglike =   community_ext.model_log_likelihood(G,partition,model=method,pars={'gamma':best_par})
+    return partition, loglike, best_par
+
+
+fn1 = "datasets/polblogs/polblogs.edges"
+fn2 = fn1.replace(".edges",".clusters")
+print "DATASET:",fn1
+
+# load graph
+G = nx.Graph()
+for line in open(fn1):
+    from_node, to_node = map(int, line.rstrip().split("\t"))
+    if from_node not in G or to_node not in G[from_node]:
+        G.add_edge(from_node,to_node)
+
+# load the ground-truth partition
+groundtruth_partition = dict()
+for line in open(fn2):
+    node, cluster = map(int, line.rstrip().split("\t"))
+    if node not in G.nodes(): continue 
+    groundtruth_partition[node] = cluster
+
+# print some general info
+gt_mu = community_ext.estimate_mu(G,groundtruth_partition)
+print "ground truth mu\t",gt_mu
+print "ground truth clusters\t",len(set(groundtruth_partition.values()))
+print "ground truth modularity\t", community_ext.modularity(groundtruth_partition,G)
+
+# now cycle thru the methods and optimize with each one
+methods = ('ppm','dcppm','ilfrs','ilfr')
+
+for method in methods:
+    print '\nMethod', method
+    # a starting parameter value depends on the method
+    if method in ('ilfrs','ilfr'):
+        work_par = 0.5
+    else:
+        work_par = 1.
+
+    partition, loglike, best_par = opt_par(G,method,work_par)
+    # calculate and print the scores of resulting partition
+    part_scores = community_ext.compare_partitions(groundtruth_partition,partition)
+    print 'best par',best_par
+    print "rand\t% 0f\tjaccard\t% 0f\tnmi\t% 0f\tsize\t%d\tloglike\t% 0f" %\
+            (part_scores['rand'], part_scores['jaccard'], part_scores['nmi'], len(set(partition.values())), loglike)
---
+++ example_run_ilfr.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import community_ext
+import networkx as nx
+import scipy
+
+def opt_mu(G,partition,mu):
+    opt_fn = lambda x:-community_ext.ilfr_mu_loglikelihood(G,partition,current_mu=x[0],model='ilfr')
+    return float(scipy.optimize.fmin_powell(opt_fn, mu,full_output = False,disp = False))
+
+
+fn1 = "datasets/polblogs/polblogs.edges"
+fn2 = fn1.replace(".edges",".clusters")
+print "DATASET:",fn1
+
+# load graph
+G = nx.Graph()
+for line in open(fn1):
+    from_node, to_node = map(int, line.rstrip().split("\t"))
+    if from_node not in G or to_node not in G[from_node]:
+        G.add_edge(from_node,to_node)
+
+# load the ground-truth partition
+groundtruth_partition = dict()
+for line in open(fn2):
+    node, cluster = map(int, line.rstrip().split("\t"))
+    if node not in G.nodes(): continue 
+    groundtruth_partition[node] = cluster
+
+# print some general info
+gt_mu = community_ext.estimate_mu(G,groundtruth_partition)
+print "ground truth mu\t",gt_mu
+print "ground truth clusters\t",len(set(groundtruth_partition.values()))
+print "ground truth modularity\t", community_ext.modularity(groundtruth_partition,G)
+
+method = 'ilfr'
+print '\nMethod', method
+
+# a starting parameter value depends on the method
+work_par = 0.5
+
+# now start the iterative process    
+prev_par, it = -1, 0
+prev_pars = set()
+while abs(work_par-prev_par)>1e-5: # stop if the size of improvement too small
+    it += 1
+    if it>100: break # stop after 100th iteration
+
+    # update the parameter value
+    prev_par = work_par
+    if prev_par in prev_pars: break # stop if we are in the cycle
+    prev_pars.add(prev_par)
+
+    # find the optimal partition with the current parameter value
+    partition = community_ext.best_partition(G,model=method,pars={'mu':work_par})
+
+    # calculate optimal parameter value for the current partition
+    work_par = opt_mu(G,partition,community_ext.estimate_mu(G,partition))
+
+    loglike =   community_ext.model_log_likelihood(G,partition,model=method,pars={'gamma':work_par,'mu':work_par})
+    print 'current par',work_par,'loglike',loglike
+
+# calculate and print the scores of resulting partition
+part_scores = community_ext.compare_partitions(groundtruth_partition,partition)
+loglike =   community_ext.model_log_likelihood(G,partition,model=method,pars={'gamma':work_par,'mu':work_par})
+print 'best par',work_par
+print "rand\t% 0f\tjaccard\t% 0f\tnmi\t% 0f\tsize\t%d\tloglike\t% 0f" %\
+        (part_scores['rand'], part_scores['jaccard'], part_scores['nmi'], len(set(partition.values())), loglike)
